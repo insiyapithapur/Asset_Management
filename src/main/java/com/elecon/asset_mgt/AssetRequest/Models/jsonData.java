377a474e9d4c6c22f562bb4375fe3565b96f49f9
insiyapithapur <banupithapur@gmail.com>
2024-02-23 10:56:26 +0530

    updatedOne

---
+++ src/main/java/com/elecon/asset_mgt/AssetRequest/Models/jsonData.java
@@ -0,0 +1,16 @@
+package com.elecon.asset_mgt.AssetRequest.Models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
+@Entity
+@Table(name = "JsonData")
+public class jsonData {
+
+}
